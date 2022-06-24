186dbce1b98debfcf5b5821dce59e756b4d659c2
Rancor38 <zak.rancor@hotmail.com>
2022-06-24 14:15:37 -0700

    commented out carousel buttons in JS and CSS, as Navbar is sufficient

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
 
   const buttons = carousel.querySelectorAll(".carousel__button")
 
-  buttons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      // un-select all the items
-      items.forEach((item) =>
-        item.classList.remove("carousel__item--selected")
-      );
-      buttons.forEach((button) =>
-        button.classList.remove("carousel__button--selected")
-      );
+//   buttons.forEach((button, i) => {
+//     button.addEventListener("click", () => {
+//       // un-select all the items
+//       items.forEach((item) =>
+//         item.classList.remove("carousel__item--selected")
+//       );
+//       buttons.forEach((button) =>
+//         button.classList.remove("carousel__button--selected")
+//       );
 
-      items[i].classList.add("carousel__item--selected")
-      button.classList.add("carousel__button--selected")
-    })
-  })
+//       items[i].classList.add("carousel__item--selected")
+//       button.classList.add("carousel__button--selected")
+//     })
+//   })
 
   // Select the first item on page load
   items[0].classList.add("carousel__item--selected")
